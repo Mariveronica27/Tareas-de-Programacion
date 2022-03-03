2e2ead1b3c43f3f5a022ce1ad9b35c0eb62180e1
Mariveronica27 <99775336+Mariveronica27@users.noreply.github.com>
2022-03-03 13:36:19 -0800

    Tarea en lenguaje Python
    puse todas las operaciones juntas

---
+++ Prueba.py
@@ -0,0 +1,17 @@
+variable1 = input("Ingrese variable1: ")
+variable1 = int(variable1)
+
+variable2 = input("Ingrese variable2: ")
+variable2 = int(variable2)
+
+suma = variable1 + variable2
+resta = variable1 - variable2
+division = variable1 / variable2
+multiplicacion = variable1 * variable2
+print(f"Resultado de la suma: {suma}")
+print(f"Resultado de la resta: {resta}")
+print(f"Resultado de la division: {division}")
+print(f"Resultado de la multiplicacion: {multiplicacion}")
+
+nombre = "Maria Arismendi"
+print(nombre)
