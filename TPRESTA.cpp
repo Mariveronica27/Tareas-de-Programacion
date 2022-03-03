178a76f81099e10b324eac9e618cbf60592930b3
Mariveronica27 <99775336+Mariveronica27@users.noreply.github.com>
2022-03-03 13:35:02 -0800

    TAREAS EN LENGUAJE C++

---
+++ TPRESTA.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number1;
+   int number2;
+   
+   cout << "ingrese el primer numero : " ;
+   cin >> number1;
+   
+   cout << "ingrese el segundo numero : " ;
+   cin >> number2;
+   
+   cout << "Resultado de resta: " << (number1 - number2) << endl;
+
+    return 0;
+}
