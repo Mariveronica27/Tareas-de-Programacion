ce58a28d2af4ce94e9a31932b84894497243b619
Mariveronica27 <99775336+Mariveronica27@users.noreply.github.com>
2022-03-03 18:33:13 -0800

    Update TPCALCULADORAC++.cpp

--- TPCALCULADORAC++.cpp
+++ TPCALCULADORAC++.cpp
@@ -1,11 +1,4 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <iostream>
 
 using namespace std;
@@ -38,7 +31,7 @@ int main()
         }
         
         case 2: {
-            cout << "Resultado de resta: " << (number1 + number2) << endl;
+            cout << "Resultado de resta: " << (number1 - number2) << endl;
             break;
         }
         
